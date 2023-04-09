596352a64998b2a15ca3d6d1ce8979e522040f8e
Nikki NoLife <nikkinolife@gmail.com>
2023-04-10 02:18:24 +0400

    feat: add tests for components and pages

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,4 @@
-// TODO INSERT YOUR API KEY HERE!!!
-export const FLICKR_API_KEY = process.env.FLICKR_API_KEY;
+export const FLICKR_API_KEY = '53d80c0e75ecda6105073797bc211dc4'; //TODO it is not a good idea to keep the key in the code, but for the sake of simplicity I will leave it here for cross-checking
 
 export const API_BASE_URL = 'https://www.flickr.com/services/rest/';
 
